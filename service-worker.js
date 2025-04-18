7af4dd01a00ef4d860735b553b55caa0e8f21729
Gricelle Perez <41925770+gpere190@users.noreply.github.com>
2025-04-18 18:24:11 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('notepad-cache').then((cache) => {
-      return cache.addAll(['/', '/index.html', '/style.css', '/app.js']);
+      return cache.addAll(['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon.png' ]);
     })
   );
 });
